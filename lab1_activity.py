f3a61b74544a223d3611c7bce89e039dbb4ff103
Bahubali majagavi <bahubalimajagavi2020@gmail.com>
2025-06-26 21:08:59 +0530

    Add files via upload

---
+++ lab1_activity.py
@@ -0,0 +1,29 @@
+#1st QN
+n=int(input("Enter a number:"))
+if n%2==0:
+    print("number is even")
+else:
+    print("number is odd")
+
+#2nd QN
+sub1=int(input("Enter subj1 marks:"))
+sub2=int(input("Enter subj2 marks:"))
+sub3=int(input("Enter subj3 marks:"))
+sub4=int(input("Enter subj4 marks:"))
+sub5=int(input("Enter subj5 marks:"))
+total=sub1+sub2+sub3+sub4+sub5
+avg=total/5
+print(avg)
+
+#4th QN
+id=int(input("Enter your id:"))
+name=(input("Enter your name:"))
+age=int(input("Enter you age:"))
+city=(input("Enter your city:"))
+phone_number=int(input("Enter your phone number:"))
+print(id)
+print(name)
+print(age)
+print(city)
+print(phone_number)
+
